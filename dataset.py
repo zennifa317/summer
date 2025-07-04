88f17462a547b29acc5f0adcb274d2311103b8d4
zenifa317 <zenifa2001317@gmail.com>
2025-07-04 20:46:53 +0900

    feat: VTDsクラスをdataset.pyに追加
    - dataset.pyにVTDsクラスを実装しました。
    - データとラベルを受け取り、変換を適用するオプションを提供します。
    - データセットの長さを取得するメソッドと、インデックスによるデータ取得メソッドを追加しました。

---
+++ dataset.py
@@ -0,0 +1,20 @@
+import torch
+from torch.utils.data import Dataset
+
+class VTDs(Dataset):
+    def __init__(self, data, labels, transform=None):
+        self.data = data
+        self.labels = labels
+        self.transform = transform
+
+    def __len__(self):
+        return len(self.data)
+
+    def __getitem__(self, idx):
+        data = self.data[idx]
+        labels = self.labels[idx]
+
+        if self.transform:
+            data = self.transform(data)
+
+        return data, labels
